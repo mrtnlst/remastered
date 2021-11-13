7ce26fe12cd7ec142f9652800b5d284522daf530
Martin List <martinlist@me.com>
2021-11-13 12:22:27 +0100

    Initial Commit

---
+++ Remastered/RemasteredApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemasteredApp.swift
+//  Remastered
+//
+//  Created by martin on 13.11.21.
+//
+
+import SwiftUI
+
+@main
+struct RemasteredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
